36c94562085e0cbe211514fb03f95aeffa9ca5cd
Bearded Avenger <email@nickhaskins.com>
2014-12-08 18:51:04 -0600

    helpers and doc blocks

--- public/includes/helpers.php
+++ public/includes/helpers.php
@@ -2,8 +2,31 @@
 
 /**
 *
+*	Get the number of votes for a specific idea
+*
+*	@param $postid int id of the post to retrieve votes for
+*	@return number of votes
+*	@since 1.0
+*/
+function idea_factory_get_votes( $postid = 0 ) {
+
+	if ( empty( $postid ) )
+		return;
+
+	$votes = get_post_meta( $postid, '_idea_votes', true );
+
+	return !empty( $votes ) ? $votes : false;
+}
+
+/**
+*
 *	Grab an optoin from our settings
 *
+*	@param $option string name of the option
+*	@param $section string name of the section
+*	@param $default string/int default option value
+*	@return the option value
+*	@since 1.0
 */
 function idea_factory_get_option( $option, $section, $default = '' ) {
 
