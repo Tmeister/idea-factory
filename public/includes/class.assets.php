ce075108f59027ac3e2118f29c5fd410d14e8a7f
Bearded Avenger <email@nickhaskins.com>
2014-12-08 18:54:59 -0600

    hook up option to disable css

--- public/includes/class.assets.php
+++ public/includes/class.assets.php
@@ -12,13 +12,18 @@ class ideaFactoryAssetLoader {
 	}
 
 	function scripts(){
+
+		$disable_css    = idea_factory_get_option('if_disable_css','if_settings_advanced');
 		$url 			= isset( $_SERVER['REQUEST_URI'] ) ? $_SERVER['REQUEST_URI'] : null;
 		$is_empty_idea 	= substr($url,-6) == '/ideas' || substr($url,-7) == '/ideas/';
 
 	    if ( 'ideas' == get_post_type() || $is_empty_idea ):
 
-	    	wp_enqueue_style('dashicons');
-	    	wp_enqueue_style('idea-factory-css', IDEA_FACTORY_URL.'/public/assets/css/idea-factory.css', IDEA_FACTORY_VERSION, true );
+	    	if ( 'on' !== $disable_css ) {
+	    		wp_enqueue_style('dashicons');
+	    		wp_enqueue_style('idea-factory-css', IDEA_FACTORY_URL.'/public/assets/css/idea-factory.css', IDEA_FACTORY_VERSION, true );
+			}
+
 			wp_enqueue_script('idea-factory-script', IDEA_FACTORY_URL.'/public/assets/js/idea-factory.js', array('jquery'), IDEA_FACTORY_VERSION, true);
 
 			wp_localize_script('idea-factory-script', 'idea_factory', array(
